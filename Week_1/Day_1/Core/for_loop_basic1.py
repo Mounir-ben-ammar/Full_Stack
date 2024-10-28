e32f58cac8fb70fca3981007c27698c9b343249f
mounir ben ammar <mounir.ben.ammar2@gmail.com>
2024-10-28 17:51:10 +0100

    For Loop Basic

--- Week_1/Day_1/Core/for_loop_basic1.py
+++ Week_1/Day_1/Core/for_loop_basic1.py
@@ -1,11 +1,33 @@
-for i  in range  (151):
-    print (i)
+# for i  in range  (151):
+#     print (i)
 
-for a  in range  (0, 1001, 5):
-    print (a)
+# for a  in range  (0, 1001, 5):
+#     print (a)
 
 
-for a  in range  (0, 101, 5):
-    if a / 5 :
-        if a
-    print (a)
+# for a  in range  (0, 101, 1):
+#     if a % 10 == 0:
+#         print ("Coding Dojo")
+#     elif  a % 5 ==0 :
+#         print ("coding")
+#     else :
+#         print(a)
+
+# somme = 0
+# for a  in range  (1, 500001):
+#     if a % 2 != 0:
+#         somme = somme + a
+
+# print( somme)
+
+
+
+# for a  in range  (2018, 0, -4):
+#     print(a)
+
+lowNum =5
+highNum =200
+mult=3
+
+for a  in range  (lowNum, highNum, mult):
+    print(a)
