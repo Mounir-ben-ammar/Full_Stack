8e1399b4e8a537d859d2cd065e792db5cf1da473
mounir ben ammar <mounir.ben.ammar2@gmail.com>
2024-10-29 17:44:25 +0100

    MySQL workbench setup

--- Week_1/Day_1/Practice/functions_Basic_ii.py
+++ Week_1/Day_1/Practice/functions_Basic_ii.py
@@ -17,5 +17,18 @@
 #     return a[1]
 # print (print_and_return([1,2]))
 
-def first_plus_length(list):
-    print (l)
+# def first_plus_length(list):
+#     print (l)
+
+
+
+def multiply(num_list, num):
+    for x in num_list:
+        x *= num
+    return num_list
+a = [2,4,10,16]
+b = multiply(a,5)
+print(b)
+# output:
+
+
